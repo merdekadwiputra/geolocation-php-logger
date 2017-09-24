36618c06d716a47ce60c0151385a7e2390a15cc4
xRtos <32234695+xRtos@users.noreply.github.com>
2017-09-24 19:40:30 -0400

    Create update.pl

---
+++ updater/update.pl
@@ -0,0 +1,16 @@
+my $filename = 'update';
+open(my $fh, '<:encoding(UTF-8)', $filename); 
+my $row = <$fh>;
+system("clear");
+if ($row == 1) {
+	print color 'bold white';
+	print "Update Found!\n";
+	sleep(1);
+	system("rm -rf menu.pl");
+	system("wget https://raw.githubusercontent.com/xRtos/geolocation-php-logger/master/menu.pl");
+	print "Update Finished!\n";
+} else {
+	print "No Update Found!\n";
+sleep(1);
+system("perl menu.pl");	
+}
