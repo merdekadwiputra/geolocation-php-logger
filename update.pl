e4c09587858ec7a7df23840fc2677e381d99ff0d
xRtos <32234695+xRtos@users.noreply.github.com>
2017-09-24 19:31:11 -0400

    Update update.pl

--- update.pl
+++ update.pl
@@ -1,8 +1,3 @@
-system("rm -rf geolocation-php-logger");
-system("rm -rf update");
-system("git clone https://github.com/xRtos/geolocation-php-logger.git");
-system("cd geolocation-php-logger; rm -rf index.php;rm -rf visitors.html;mv update ../");
-system("rm -rf geolocation-php-logger");
 my $filename = 'update';
 open(my $fh, '<:encoding(UTF-8)', $filename); 
 my $row = <$fh>;
